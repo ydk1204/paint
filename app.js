d1f31b66a18654e3e06cb8e298ff25421a4e6708
ydk1204 <ejrbdi@naver.com>
2022-05-25 00:15:29 +0900

    paint modeChange and save

--- app.js
+++ app.js
@@ -1,16 +1,26 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext('2d');
 const colors = document.getElementsByClassName('jsColor');
+const range = document.getElementById('jsRange');
+const mode = document.getElementById('jsMode');
+const saveBtn = document.getElementById('jsSave');
 
-canvas.width = 700;
-canvas.height = 700;
+const INITIAL_COLOR = "#2c2c2c";
+const CANVAS_SIZE = 700;
 
-ctx.strokeStyle = "#2c2c2c";
+canvas.width = CANVAS_SIZE;
+canvas.height = CANVAS_SIZE;
+
+ctx.fillStyle = "white";
+ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+ctx.strokeStyle = INITIAL_COLOR;
+ctx.fillStyle = INITIAL_COLOR;
 ctx.lineWidth = 2.5;
 
 let painting = false;
+let filling = false;
 
-const stopPainting = () => {
+const stopPainting = () => { 
   painting = false;
 }
 
@@ -21,18 +31,56 @@ const startPainting = () => {
 const onMouseMove = (e) => {
   const x = e.offsetX;
   const y = e.offsetY;
-  if (!painting) {
+  if (!filling) {
+    if (!painting) {
     ctx.beginPath();
     ctx.moveTo(x, y);
   } else {
     ctx.lineTo(x, y);
     ctx.stroke();
   }
+  }
 }
 
 const handleColorClick = (e) => {
   const color = e.target.style.backgroundColor;
   ctx.strokeStyle = color;
+  ctx.fillStyle = color;
+}
+
+const handleRangeChange = (e) => {
+  const size = e.target.value;
+  ctx.lineWidth = size;
+}
+
+const handleModeClick = (e) => {  
+  if (filling) {
+    filling = false;
+    mode.innerText = 'Fill';
+  }
+  else {
+    filling = true;
+    mode.innerText = 'Paint';
+  }
+}
+
+const handleCanvasClick = () => {
+  if (filling) {
+    ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+  }
+}
+
+const handleCM = (e) => {
+  // alert('우클릭 방지');
+  e.preventDefault();
+}
+
+const handleSaveClick = () => {
+  const image = canvas.toDataURL('image/png');
+  const link = document.createElement("a");
+  link.href = image;
+  link.download = 'PaintImage';
+  link.click();
 }
 
 if (canvas) {
@@ -40,6 +88,23 @@ if (canvas) {
   canvas.addEventListener("mousedown", startPainting);
   canvas.addEventListener("mouseup", stopPainting);
   canvas.addEventListener('mouseleave', stopPainting);
+  canvas.addEventListener('click', handleCanvasClick);
+  canvas.addEventListener("contextmenu", handleCM);
 }
 
-Array.from(colors).forEach(color => color.addEventListener('click', handleColorClick))
+if (colors) {
+  Array.from(colors).forEach(color => color.addEventListener('click', handleColorClick))
+}
+
+if (range) {
+  range.addEventListener("input", handleRangeChange);
+}
+
+if (mode) {
+  mode.addEventListener('click', handleModeClick)
+}
+
+if (saveBtn) {
+  saveBtn.addEventListener('click', handleSaveClick);
+}
+
