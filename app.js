a2b56455943c02aefa75945c1a3a529298c4265f
ydk1204 <ejrbdi@naver.com>
2022-05-24 22:47:23 +0900

    create line

---
+++ app.js
@@ -0,0 +1,41 @@
+const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext('2d');
+
+canvas.width = 700;
+canvas.height = 700;
+
+ctx.strokeStyle = "#2c2c2c";
+ctx.lineWidth = 2.5;
+
+let painting = false;
+
+const stopPainting = () => {
+  painting = false;
+}
+
+const startPainting = () => {
+  painting = true;
+}
+
+const onMouseMove = (e) => {
+  const x = e.offsetX;
+  const y = e.offsetY;
+  if (!painting) {
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+  } else {
+    ctx.lineTo(x, y);
+    ctx.stroke();
+  }
+}
+
+const onMouseDown = (e) => {
+  painting = true;
+}
+
+if (canvas) {
+  canvas.addEventListener("mousemove", onMouseMove);
+  canvas.addEventListener("mousedown", startPainting);
+  canvas.addEventListener("mouseup", stopPainting);
+  canvas.addEventListener('mouseleave', stopPainting);
+}
