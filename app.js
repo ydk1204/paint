2dec554fc85d49e17e661318e769be758ae73ee3
ydk1204 <ejrbdi@naver.com>
2022-05-24 22:58:46 +0900

    change color

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext('2d');
+const colors = document.getElementsByClassName('jsColor');
 
 canvas.width = 700;
 canvas.height = 700;
@@ -29,8 +30,9 @@ const onMouseMove = (e) => {
   }
 }
 
-const onMouseDown = (e) => {
-  painting = true;
+const handleColorClick = (e) => {
+  const color = e.target.style.backgroundColor;
+  ctx.strokeStyle = color;
 }
 
 if (canvas) {
@@ -39,3 +41,5 @@ if (canvas) {
   canvas.addEventListener("mouseup", stopPainting);
   canvas.addEventListener('mouseleave', stopPainting);
 }
+
+Array.from(colors).forEach(color => color.addEventListener('click', handleColorClick))
